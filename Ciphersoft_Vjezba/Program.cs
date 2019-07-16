79a64881c3a760dc68aaf89fb0edc76f32ad5040
Shobster <danilosimeunovic2@gmail.com>
2019-07-16 18:36:32 +0200

    Add project files.

---
+++ Ciphersoft_Vjezba/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Ciphersoft_Vjezba
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Department> Lista { get; set; }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlogDbContext BDC = new BlogDbContext();
+
+            List<Department> Depts = BDC.Lista.ToList();
+
+            Department D = new Department { Name = "Danilo", Surname = "Simeunovic" };
+            BDC.Lista.Add(D);
+
+            BDC.SaveChanges();
+        }
+    }
+}
